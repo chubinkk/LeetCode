9a1651d114284872a4e23af71dd2247b42c8f6ac
chubinkk <33720822+chubinkk@users.noreply.github.com>
2022-01-25 00:36:46 +0900

    Time: 62 ms (41.42%), Space: 15.5 MB (23.56%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -1,7 +1,18 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         s = s.lower()
-        
         s = re.sub('[^a-z0-9]', '', s)
         
         return s == s[::-1]
+
+# Solution 2
+#        s_list = []
+#        for i in s:
+#            if i.isalnum():
+#                s_list.append(i.lower())
+#        
+#        while len(s_list) > 1:
+#            if s_list.pop(0) != s_list.pop():
+#                return False
+#        
+#        return True
